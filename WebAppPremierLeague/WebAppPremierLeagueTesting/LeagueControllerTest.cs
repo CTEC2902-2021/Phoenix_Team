17bd5b54a8293ede92592a0e2297493a70f0f37b
Dexter Wilkins <P2569728@my365.dmu.ac.uk>
2021-03-22 23:35:51 +0000

    Dexter : Added testing and LeagueControllerTest.cs

---
+++ WebAppPremierLeague/WebAppPremierLeagueTesting/LeagueControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+
+namespace WebAppPremierLeagueTesting
+{
+    [TestClass]
+    public class LeagueControllerTest
+    {
+        [TestMethod]
+        public void IndexWelcomeMessage()
+        {
+            //arrange
+           
+        }
+    }
+}
