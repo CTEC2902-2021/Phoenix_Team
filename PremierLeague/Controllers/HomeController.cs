c7036ad1e8bd97f5005163a496f61944b1f0da79
Joe Jordan <79266458+Jo3mat3@users.noreply.github.com>
2021-03-22 20:59:03 +0000

    Add files via upload

---
+++ PremierLeague/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using PremierLeague.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PremierLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            LeagueModel league = new LeagueModel()
+            {
+                Title = "The Premier League",
+                ByLine = "For all the latest Premier League news."
+            };
+
+            return View(league);
+        }
+    }
+}
---
+++ PremierLeague/Models/LeagueModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PremierLeague.Models
+{
+    public class LeagueModel
+    {
+        public string Title { get; set; }
+        public string ByLine { get; set; }
+        public string Content { get; set; }
+    }
+}
