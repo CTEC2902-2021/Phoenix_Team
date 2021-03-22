01b68982495d55d619f46c2af42de4143f108320
Joseph Jordan <P2536634@my365.dmu.ac.uk>
2021-03-22 22:07:43 +0000

    Added HomeController

---
+++ WebAppPremierLeague/WebAppPremierLeague/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppPremierLeague.Models;
+
+namespace WebAppPremierLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            LeagueModel league = new LeagueModel()
+            {
+                Title = "The Premier League",
+                SubTitle = " For all the latest Premier League news.",
+
+            };
+
+            return View(league);
+        }
+    }
+}
