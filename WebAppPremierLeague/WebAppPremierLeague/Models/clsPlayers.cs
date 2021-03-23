52f53d372ce219cf13ced322941eb1d279d77859
Dexter Wilkins <P2569728@my365.dmu.ac.uk>
2021-03-23 13:33:24 +0000

    Dexter : renamed classes.

---
+++ WebAppPremierLeague/WebAppPremierLeague/Models/clsPlayers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPremierLeague.Models
+{
+    public class clsPlayers
+    {
+        public virtual int PlayerNo { get; set; }
+        public virtual int TeamNo { get; set; }
+        public virtual string PlayerName { get; set; }
+        public virtual int JerseryNumber { get; set; }
+        public virtual int NoOfFans { get; set; }
+
+        
+    }
+}
