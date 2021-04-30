c932f0a8431c53618fc58d50a22c7e817d806838
Dexter Wilkins <P2569728@my365.dmu.ac.uk>
2021-04-30 15:00:54 +0100

    added teamsmodel code, and timer to homepage

---
+++ WebAppPremierLeague/WebAppPremierLeague/Models/TeamsModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPremierLeague.Models
+{
+    public class TeamsModel : Controller
+    {
+
+        public int TeamNo { get; set; }
+        public string TeamName { get; set; }
+        public string TeamManager { get; set; }
+        public int Draws { get; set; }
+        public int GoalDifference { get; set; }
+        public int GoalsAganist { get; set; }
+        public int GoalsFore { get; set; }
+        public int Losses { get; set; }
+        public int MatchesPlayed { get; set; }
+        public int NoOfPlayers { get; set; }
+        public int Wins { get; set; }
+
+        // GET: TeamsModel
+        public ViewResult Team()
+        {
+            TeamsModel teamsmodel = new TeamsModel();
+            teamsmodel.TeamName = "Man City";
+            teamsmodel.TeamManager = "Steve Smith";
+            teamsmodel.Wins = 10;
+
+            if(teamsmodel.Wins > 20)
+            {
+                ViewBag.Subtitle = "This team has the Most Wins!";
+            }
+            else
+            {
+                ViewBag.Subtitle = "This team is Looking to get more wins";
+            }
+
+            return View(teamsmodel);
+        }
+    }
+}
